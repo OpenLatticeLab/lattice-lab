484d5390717fe5997a46f7b10613b23bd2af9d9f
guozijn <guozijn@gmail.com>
2025-09-14 20:54:08 +0930

    fix: add the type of Download for ts
    Signed-off-by: guozijn <guozijn@gmail.com>

--- src/types/mp-react-components.d.ts
+++ src/types/mp-react-components.d.ts
@@ -1,5 +1,24 @@
 declare module '@materialsproject/mp-react-components' {
   import * as React from 'react'
-  export const CrystalToolkitScene: React.ComponentType<{ data: any; [key: string]: any }>
-}
 
+  export const CrystalToolkitScene: React.ComponentType<{
+    data: any
+    [key: string]: any
+  }>
+
+  // Lightweight typing for Download component (from library sources)
+  export const Download: React.ComponentType<{
+    id?: string
+    data?: {
+      filename: string
+      content: any
+      isBase64?: boolean
+      isDataURL?: boolean
+      mimeType?: string
+    }
+    isBase64?: boolean
+    isDataURL?: boolean
+    mimeType?: string
+    [key: string]: any
+  }>
+}
